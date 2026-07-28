faa06e9ef483e3f04bd63efa2301add258eabe00
Ayordeji <ayoeluyemi2002@gmail.com>
2026-07-28 12:38:42 +0100

    Remove test scripts

--- test-ids.ts
+++
@@ -1,9 +0,0 @@
-import { db } from './src/lib/db'
-
-async function test() {
-  const users = await db.user.findMany({
-    select: { id: true, invitationId: true }
-  });
-  console.log(users.map(u => u.invitationId).filter(Boolean));
-}
-test().finally(() => process.exit(0))
